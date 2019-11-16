d1ebe8988e77f265479241561ef9a24491aef3f3
Ray <donglei19980620@gmail.com>
2019-11-16 17:52:31 +0800

    'a'

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -3,12 +3,33 @@
 		<view class="bg">
 			<view class="top">
 				<view class="search">
-					<input type="text" value="" />
-					<button type="button"> </button>
+					<view class="search-wrap">
+						<view class="magnifier">
+							<image src="../../static/icon/fangda.png" mode=""></image>
+						</view>
+						<view class="search-input-wrap">
+							<input type="text" value="" />
+						</view>
+						<view class="message"></view>
+					</view>
 				</view>
 			</view>
-			<view class="swiper">
-				
+			<view class="hh_swiper">
+				<view class="head-swiper">
+					<view class="swiper-wrap">
+						<view class="uni-padding-wrap">
+							<view class="page-section swiper">
+								<view class="page-section-spacing">
+									<swiper class="swiper" :indicator-dots="indicatorDots" :autoplay="autoplay" :interval="interval" :duration="duration">
+										<swiper-item v-for="(img, key) in background" :key="key" :style="{backgroundImage: 'url(' + img + ')' }">
+											<view class="swiper-item"></view>
+										</swiper-item>
+									</swiper> 
+								</view>
+							</view>
+						</view>
+					</view>
+				</view>
 			</view>
 			<view class="recommend">
 				
@@ -24,7 +45,11 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				background: ['../../static/swiper/1.png', '../../static/swiper/2.png', '../../static/swiper/3.png'],
+				indicatorDots: true,
+				autoplay: true,
+				interval: 4000,
+				duration: 500
 			}
 		},
 		onLoad() {
@@ -41,26 +66,73 @@
 		width: 100%;
 		display: flex;
 		flex-direction: column;
+		background-color: #ff5a00;
 		.top{
+			display: flex;
+			flex-direction: row;
+			justify-content: center;
 			width: 100%;
-			background-color: #007AFF;
+			//background-color: #007AFF;
+			padding-top: 20rpx;
 			.search{
-				padding: 20rpx 20rpx 20rpx 20rpx;
-				width: 100%;
+				padding: 5rpx 10rpx 15rpx 10rpx;
+				width: 92%;
+				height: 30rpx;
 				display: flex;
 				flex-direction: row;
-				input{
-					width: 80%;
-					border-radius: 10rpx;
-				}
-				button{
-					width: 10%;
+				border-radius: 35rpx;
+				background-color: #F8F8F8;
+				.search-wrap{
+					width: 100%;
+					display: flex;
+					flex-direction: row;
+					justify-content: space-between;
+					.magnifier{
+						width: 40rpx;
+						height: 40rpx;
+						// background-color: red;
+						image{
+							display: block;
+							width: 40rpx;
+							height: 40rpx;
+						}
+					}
+					.search-input-wrap{
+						width: 80%;
+						height: 40rpx;
+						border-radius: 10rpx;
+						// background-color: pink;
+						input{
+							height: 40rpx;
+							min-height: 40rpx;
+							font-size: 25rpx;
+						}		
+					}
+
+					.message{
+						width: 10%;
+						height: 35rpx;
+					}
 				}
 			}
 		}
-		.swiper{
+		.hh_swiper{
 			width: 100%;
-			background-color: #4CD964;
+			display: flex;
+			flex-direction: row;
+			justify-content: center;
+			margin-top: 20rpx;
+			.head-swiper{
+				width: 95%;
+				background-color: #4CD964;
+				border-radius: 25rpx;
+				.swiper-wrap{
+					width: 100%;
+					.uni-padding-wrap{
+						width: 100%;
+					}
+				}
+			}
 		}
 		.recommend{
 			width: 100%;
@@ -71,4 +143,5 @@
 			background-color: pink;
 		}
 	}
+
 </style>
