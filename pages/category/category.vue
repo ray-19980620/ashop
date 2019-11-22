c0b3235bfc5e60aca345067c2fc7b17873017968
Ray <ray@Rays-Mac-mini.local>
2019-11-22 21:03:43 +0800

    vant

--- pages/category/category.vue
+++ pages/category/category.vue
@@ -1,6 +1,8 @@
 <template>
 	<view>
-		
+		<van-sidebar v-model="activeKey">
+		  <van-sidebar-item v-for="(item, key) in category" :category-id="item.id" :key="key" :title="item.name" />
+		</van-sidebar>
 	</view>
 </template>
 
@@ -8,8 +10,38 @@
 	export default {
 		data() {
 			return {
-				
+				activeKey: 0,
+				category: [
+					{
+						'id': 0, 'name': '男装'
+					},
+					{
+						'id': 1, 'name': '女装'
+					},
+					{
+						'id': 2, 'name': '男鞋'
+					}
+				],
+				category_son: {
+					0: [
+						{'id': 1, 'name': '风衣'},
+						{'id': 2, 'name': '卫衣'}
+					],
+					1: [
+						{'id': 1, 'name': '套装裙'},
+						{'id': 2, 'name': '背带裤'}
+					],
+					2: [
+						{'id': 1, 'name': '休闲鞋'},
+						{'id': 2, 'name': '皮鞋'}
+					]
+				}
 			};
+		},
+		methods: {
+			change: function(e) {
+				console.log(e);
+			}
 		}
 	}
 </script>
