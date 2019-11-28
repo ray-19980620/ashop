a7bb7c9288d94af5d13680fdfa053600ec33fad8
Ray <ray@Rays-Mac-mini.local>
2019-11-28 22:20:30 +0800

    cart

--- pages/cart/cart.vue
+++ pages/cart/cart.vue
@@ -1,6 +1,20 @@
 <template>
-	<view>
-		
+	<view class="container">
+		<view class="main">
+			<van-submit-bar
+			  :price="3050"
+			  button-text="提交订单"
+			  @submit="onSubmit"
+			>
+			  <van-checkbox v-model="checked">全选</van-checkbox>
+			  <span slot="tip">
+			    你的收货地址不支持同城送, <span>修改地址</span>
+			  </span>
+			</van-submit-bar>
+		</view>
+		<view class="bottom">
+
+		</view>
 	</view>
 </template>
 
@@ -15,5 +29,14 @@
 </script>
 
 <style lang="scss">
-
+	.container{
+		position: relative;
+		.main{
+			
+		}
+		.bottom{
+			width: 100%;
+			z-index: 1;
+		}
+	}
 </style>
